573d20ea0199fe9eab7edd7349328316dbcd31f0
fdy696 <3456283371@qq.com>
2018-09-16 20:33:16 +0800

    缓动

--- gotop/goTop.js
+++ gotop/goTop.js
@@ -7,23 +7,29 @@
       this.bind()
     }
     GoTop.prototype.init = function(){
-      this.$target = $('<li class="go-top"><a href="javascript: void(0)">GO</a></li>').css({display:"none"});
+      this.$target = $('<div class="go-top">GO</div>').css({display:"none"});
 			this.$ct.append(this.$target);
     }
     GoTop.prototype.bind = function(){
       var self = this;
       $(window).on("scroll",function(){
         var scrollTop = $(window).scrollTop();
-        console.log(scrollTop)
-        scrollTop>100?self.$target.show():self.$target.hide();
+        scrollTop>300?self.$target.show():self.$target.hide();
       });
       this.$target.on("click",function(){
-        $(window).scrollTop(0);
+        var timer = setInterval(function(){
+          var nextScroll = $(window).scrollTop() - 200;
+            if(nextScroll<=0){
+              $(window).scrollTop(0);
+              window.clearInterval(timer)
+              return;
+            }
+          $(window).scrollTop(nextScroll);
+          },100)   
       })
     }
-
     new GoTop(this);
   }
 }()
 
-$(".sidebar ul").gotop()
+$("body").gotop()
